45f99a901ba0e333728793c3fb777930290cfa30
Kwe <Kweayon@gmail.com>
2023-07-08 19:05:10 -0500

    update: extracted author

--- src/webScraping101.py
+++ src/webScraping101.py
@@ -9,13 +9,19 @@ def getData(uri: str):
     return siteHTML
 
 def parseHTML(siteHTML):
-    quotes = siteHTML.findAll("span",attrs={'class':'text'})
-    return quotes
+    quotes = siteHTML.findAll('span',attrs={'class':'text'})
+    authors = siteHTML.findAll('small',attrs={
+        'class':'author'
+    })
+    return (quotes, authors)
 
 def main():
     uri = 'https://quotes.toscrape.com/'
     siteHTML = getData(uri)
-    print(parseHTML(siteHTML))
+    quotes, authors = parseHTML(siteHTML)
+    print(f'quotes {quotes}')
+    print(f'authors {authors}')
+
 
 if __name__ == '__main__':
     main()
